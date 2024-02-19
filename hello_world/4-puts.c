d46fa5cafbb9604fb2fb3de34e10e6e46e9973f4
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-19 14:27:54 +0000

    putz.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- * main - A program that prints a line with puts fuction
- * Return: 0 (Success)
-  */
-int main(void) {
+
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }

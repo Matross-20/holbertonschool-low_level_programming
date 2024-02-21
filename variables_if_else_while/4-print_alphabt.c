6be2d9fb3c7f2793639ad8523976d00ea07f40a9
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-21 17:44:49 +0000

    alphABET

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+/**
+ * main - print alphabets without e and q
+ * Return: string
+ */
 
 int main() {
     char letter = 'a';
 
-    // Print the alphabet excluding 'q' and 'e' using putchar
     while (letter <= 'z') {
         if (letter != 'e' && letter != 'q') {
             putchar(letter);
         }
         letter++;
     }
-
-    // Print a new line using putchar
+    
     putchar('\n');
 
     return 0;

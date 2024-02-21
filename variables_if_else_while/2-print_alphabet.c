2bb3defc422377eb291ea7e2e260f280c304037c
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-21 16:40:04 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the alphabet in lowercase
+ * Return: end product of the code 
+ */
+int main(void)
+{	
+	char letter ='a';
+	       	
+	while (letter <= 'z')	
+	{
+	        putchar (letter);
+	        letter++;
+	}
+	putchar ('\n');
+	return (0);
+}	

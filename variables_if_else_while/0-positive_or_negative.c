ee7c0ef358f57fad9ec424ed3daf023cc99ef1b6
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-21 09:03:58 +0000

    Positive or Negative?

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ if (n > 0)
          printf("%d is positive\n", n);
 else if (n == 0)
          printf("%d is zero\n", n);
-if (n < 0)
+else (n < 0)
          printf("%d is negative\n", n);
 return (0);
 }

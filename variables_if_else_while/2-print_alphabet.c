5285b4c005185159b573995c8b3fe610c2f049d7
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-21 16:52:08 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-char letter = 'a';  	
-while (letter <= 'z')	
+char letter = 'a';
+while (letter <= 'z')
 {
 putchar (letter);
 letter++;
 }
 putchar ('\n');
 return (0);
-}	
+}

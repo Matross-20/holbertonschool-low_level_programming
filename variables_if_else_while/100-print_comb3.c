43ec4dd0c20e5407515762d716819513170de755
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-22 14:02:33 +0000

    Double Combinations

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,21 +5,23 @@
  *Return: end product of the string
  */
 
-int main(void) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = i + 1; j < 10; j++) {
-            putchar('0' + i);
-            putchar('0' + j);
+int main(void) 
+{
+for (int i = 0; i < 10; i++) {
+for (int j = i + 1; j < 10; j++) {
+putchar('0' + i);
+putchar('0' + j);
 
-            if (i != 8 || j != 9) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+if (i != 8 || j != 9) 
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
 

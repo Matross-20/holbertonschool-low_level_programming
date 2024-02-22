8d3b0d00a57ea29ef82a393eda1eb1286fd81c08
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-22 17:05:02 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
-int main() {
-    printf("_putchar\n");
-    return 0;
+/**
+ * main - outputs _putchar
+ * _putchar - returns string
+ * Return: returns string
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }

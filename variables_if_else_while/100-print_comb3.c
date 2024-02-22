5e1fcb144a4ab22cd07852acdcc1ed0473595433
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-22 14:07:14 +0000

    Double Combinations

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,8 +7,10 @@
 
 int main(void) 
 {
-for (int i = 0; i < 10; i++) {
-for (int j = i + 1; j < 10; j++) {
+for (int i = 0; i < 10; i++) 
+{
+for (int j = i + 1; j < 10; j++) 
+{	
 putchar('0' + i);
 putchar('0' + j);
 

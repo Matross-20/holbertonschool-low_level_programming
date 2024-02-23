5c09a93ee896e60375314bce3ecd566141853417
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 15:07:06 +0000

    98 Battery Street

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,22 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *print_to_98 - print integer
- *@n: variable name
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from n to 98
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
-for (n = 0; n <= 98; n++)
+if (n < 98)
 {
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", 98);
 }
 }
--- functions_nested_loops/11-print_to_98.s
+++ functions_nested_loops/11-print_to_98.s
@@ -1,5 +1,11 @@
 	.file	"11-print_to_98.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"%d, "
+.LC1:
+	.string	"%d\n"
+	.text
 	.globl	print_to_98
 	.type	print_to_98, @function
 print_to_98:
@@ -13,38 +19,43 @@ print_to_98:
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
 	movl	%edi, -4(%rbp)
-	movl	$0, -4(%rbp)
-	jmp	.L2
-.L3:
-	movl	-4(%rbp), %edx
-	movslq	%edx, %rax
-	imulq	$1717986919, %rax, %rax
-	shrq	$32, %rax
-	sarl	$2, %eax
-	movl	%edx, %esi
-	sarl	$31, %esi
-	subl	%esi, %eax
-	movl	%eax, %ecx
-	movl	%ecx, %eax
-	sall	$2, %eax
-	addl	%ecx, %eax
-	addl	%eax, %eax
-	subl	%eax, %edx
-	movl	%edx, %ecx
-	movl	%ecx, %eax
-	addl	$48, %eax
-	movsbl	%al, %eax
-	movl	%eax, %edi
-	call	_putchar@PLT
-	movl	$44, %edi
-	call	_putchar@PLT
-	movl	$32, %edi
-	call	_putchar@PLT
+	cmpl	$97, -4(%rbp)
+	jg	.L6
+	jmp	.L3
+.L4:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
 	addl	$1, -4(%rbp)
-.L2:
+.L3:
+	cmpl	$97, -4(%rbp)
+	jle	.L4
+	movl	$98, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L8
+.L7:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	subl	$1, -4(%rbp)
+.L6:
 	cmpl	$98, -4(%rbp)
-	jle	.L3
-	nop
+	jg	.L7
+	movl	$98, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L8:
 	nop
 	leave
 	.cfi_def_cfa 7, 8

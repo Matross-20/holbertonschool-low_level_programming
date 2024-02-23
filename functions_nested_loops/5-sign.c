87306a238155a5ec26eecf205015e3caa9ebab05
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 12:48:26 +0000

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,19 +5,22 @@
  *Return: what should be returned
  */
 
-int print_sign(int n) 
+int print_sign(int n)
 {
-if (n > 0) 
+if (n > 0)
 {
 _putchar('+');
-return 1;
-} else if (n == 0) 
+return (1);
+}
+else if (n == 0)
 {
 _putchar('0');
-return 0;
-} else 
+return (0);
+}
+else
 {
 _putchar('-');
-return -1;
+return (-1);
 }
 }
+

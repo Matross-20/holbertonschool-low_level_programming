096c4756cfe0fc9241c13a684c1dae70cdbf6c32
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 15:58:19 +0000

    Natural Numbers

---
+++ functions_nested_loops/102-fibonacci.s
@@ -0,0 +1,77 @@
+	.file	"102-fibonacci.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%ld, %ld"
+.LC1:
+	.string	", %ld"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	$1, -24(%rbp)
+	movq	$2, -16(%rbp)
+	movq	-16(%rbp), %rdx
+	movq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, -28(%rbp)
+	jmp	.L2
+.L3:
+	movq	-24(%rbp), %rdx
+	movq	-16(%rbp), %rax
+	addq	%rdx, %rax
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-16(%rbp), %rax
+	movq	%rax, -24(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	addl	$1, -28(%rbp)
+.L2:
+	cmpl	$47, -28(%rbp)
+	jle	.L3
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:

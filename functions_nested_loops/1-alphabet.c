74a0e53033acaa93da8d51b526138d73665914d4
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 09:14:05 +0000

    alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the alphabet, in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}

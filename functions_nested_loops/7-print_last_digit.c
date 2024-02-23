af79a1e17d1796fe3192e122b1f3793aef898a34
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 13:44:14 +0000

    Last digits

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit of a number
+ *Return:the value of the last digit
+ */
+int print_last_digit(int n) 
+{
+    int last_digit = n % 10;
+    if (last_digit < 0)
+    {
+        last_digit = -last_digit;
+    }
+
+    putchar('0' + last_digit); 
+    return last_digit;
+}

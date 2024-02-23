18747bfb406fa4b078b078aa165db5b152ecbdc3
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 12:30:57 +0000

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *int print_sign - prints the sign of a number.
+ *@n: variable name
+ *Return: what should be returned
+ */
+
+int print_sign(int n) 
+{
+    if (n > 0) 
+    {
+        _putchar('+');
+        return 1;
+    } else if (n == 0) 
+    {
+        _putchar('0');
+        return 0;
+    } else 
+    {
+       _putchar('-');
+        return -1;
+    }
+}
+

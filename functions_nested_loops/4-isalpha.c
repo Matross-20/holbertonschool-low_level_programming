4be2e5fe348a58a28093c35f5fbdbbc810394cac
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-23 10:54:28 +0000

    iSalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_isaplha - starts the function
+ *Return: end of function
+ *@c: int variable name
+ */
+int _isalpha(int c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0;
+}
+

9353e4aa058b394b95470d9734d7d49ef8851b98
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-27 09:45:00 +0000

    Numerus

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 1 up to 9
+ * @void: no parameter
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+	char cnum;
+
+	for (n = 0; n <= 9; n++)
+	{
+		cnum = n + '0';
+		_putchar(cnum);
+	}
+	_putchar('\n');
+}	
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,11 @@
 #include <unistd.h>
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int mul(int a, int b);
+
+void print_numbers(void);

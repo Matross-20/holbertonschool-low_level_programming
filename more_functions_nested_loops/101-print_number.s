8bbb4e89eea61cf0ce8d3a90aa1af5757a274833
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-28 09:07:42 +0000

    Integers

---
+++ more_functions_nested_loops/101-print_number.s
@@ -0,0 +1,80 @@
+	.file	"101-print_number.c"
+	.text
+	.globl	print_number
+	.type	print_number, @function
+print_number:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	cmpl	$0, -20(%rbp)
+	jns	.L2
+	negl	-20(%rbp)
+	movl	-20(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	movl	$95, %edi
+	call	_putchar@PLT
+.L2:
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	movl	$3435973837, %eax
+	imulq	%rdx, %rax
+	shrq	$32, %rax
+	shrl	$3, %eax
+	movl	%eax, -4(%rbp)
+	cmpl	$0, -4(%rbp)
+	je	.L3
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	print_number
+.L3:
+	movl	-20(%rbp), %ecx
+	movl	%ecx, %edx
+	movl	$3435973837, %eax
+	imulq	%rdx, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	shrl	$3, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	movl	%edx, %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	_putchar@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_number, .-print_number
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:

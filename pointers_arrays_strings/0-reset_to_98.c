bdd04be542bb86df35c28f7ed22a41ae42f4c27f
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-28 15:19:30 +0000

    98 Battery Str

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *rest_to_98 - Takes a pointer to an
+ *reset_to_98 - Takes a pointer to an
  *int and updates the value it points to to 98
  *
  *@n: An integer input pointer

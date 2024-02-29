19d8d51e11ed3539527c1471d413f57539d4dc5f
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-02-29 10:14:15 +0000

    Stringer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - Returns the length of a string.
+ *@s: The string to get the length of.
+ *Return: The length of @s.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s+++ '\0')
+	len++;
+	return (len);
+}	

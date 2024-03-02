698582778123b40900b1e6cae6ec96967fbfae02
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-03-02 14:38:01 +0000

    Rot13 Encoder

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - encodes a string by rotating the characters 13 spaces.
+ * @s: pointer to input string.
+ * Return: Returns pointer encoded string.
  */
 char *rot13(char *s)
 {
-int i = 0;
+	int i, j;
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
 
-while (s[i] != '\0')
-{
-while ((s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= 'A' && s[i] <= 'Z'))
-{
-if ((s[i] >= 'a' && s[i] <= 'm') ||
-(s[i] >= 'A' && s[i] <= 'M'))
-s[i] += 13;
-else
-s[i] -= 13;
-i++;
-}
-i++;
-}
-return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = r[j];
+				boolean = 1;
+			}
+		}
+	}
+	return (s);
 }
--- pointers_arrays_strings/100-rot13.s
+++ pointers_arrays_strings/100-rot13.s
@@ -11,110 +11,92 @@ rot13:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movq	%rdi, -24(%rbp)
-	movl	$0, -4(%rbp)
+	subq	$160, %rsp
+	movq	%rdi, -152(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movabsq	$6148631004284211022, %rax
+	movabsq	$4846508005305898838, %rdx
+	movq	%rax, -128(%rbp)
+	movq	%rdx, -120(%rbp)
+	movabsq	$5425229275903444292, %rax
+	movabsq	$8318836189426502988, %rdx
+	movq	%rax, -112(%rbp)
+	movq	%rdx, -104(%rbp)
+	movabsq	$7024060127144998260, %rax
+	movabsq	$7595434461045744482, %rdx
+	movq	%rax, -96(%rbp)
+	movq	%rdx, -88(%rbp)
+	movl	$1835821930, -80(%rbp)
+	movb	$0, -76(%rbp)
+	movabsq	$5208208757389214273, %rax
+	movabsq	$5786930140093827657, %rdx
+	movq	%rax, -64(%rbp)
+	movq	%rdx, -56(%rbp)
+	movabsq	$6365651522798441041, %rax
+	movabsq	$7378413942531512921, %rdx
+	movq	%rax, -48(%rbp)
+	movq	%rdx, -40(%rbp)
+	movabsq	$7957135325236127847, %rax
+	movabsq	$8535856707940741231, %rdx
+	movq	%rax, -32(%rbp)
+	movq	%rdx, -24(%rbp)
+	movl	$2054781047, -16(%rbp)
+	movb	$0, -12(%rbp)
+	movl	$0, -136(%rbp)
 	jmp	.L2
-.L9:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$96, %al
-	jle	.L4
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$109, %al
-	jle	.L5
-.L4:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$64, %al
-	jle	.L6
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$77, %al
-	jg	.L6
-.L5:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	leal	13(%rax), %ecx
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movl	%ecx, %edx
-	movb	%dl, (%rax)
-	jmp	.L7
-.L6:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	leal	-13(%rax), %ecx
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movl	%ecx, %edx
-	movb	%dl, (%rax)
 .L7:
-	addl	$1, -4(%rbp)
+	movb	$0, -137(%rbp)
+	movl	$0, -132(%rbp)
+	jmp	.L3
+.L6:
+	movl	-136(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-152(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %edx
+	movl	-132(%rbp), %eax
+	cltq
+	movzbl	-64(%rbp,%rax), %eax
+	cmpb	%al, %dl
+	jne	.L4
+	movl	-136(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-152(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-132(%rbp), %eax
+	cltq
+	movzbl	-128(%rbp,%rax), %eax
+	movb	%al, (%rdx)
+	movb	$1, -137(%rbp)
+.L4:
+	addl	$1, -132(%rbp)
 .L3:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$96, %al
-	jle	.L8
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$122, %al
-	jle	.L9
-.L8:
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$64, %al
-	jle	.L10
-	movl	-4(%rbp), %eax
-	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movzbl	(%rax), %eax
-	cmpb	$90, %al
-	jle	.L9
-.L10:
-	addl	$1, -4(%rbp)
+	movl	-132(%rbp), %eax
+	cltq
+	movzbl	-64(%rbp,%rax), %eax
+	testb	%al, %al
+	je	.L5
+	cmpb	$0, -137(%rbp)
+	je	.L6
+.L5:
+	addl	$1, -136(%rbp)
 .L2:
-	movl	-4(%rbp), %eax
+	movl	-136(%rbp), %eax
 	movslq	%eax, %rdx
-	movq	-24(%rbp), %rax
+	movq	-152(%rbp), %rax
 	addq	%rdx, %rax
 	movzbl	(%rax), %eax
 	testb	%al, %al
-	jne	.L3
-	movq	-24(%rbp), %rax
-	popq	%rbp
+	jne	.L7
+	movq	-152(%rbp), %rax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L9
+	call	__stack_chk_fail@PLT
+.L9:
+	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc

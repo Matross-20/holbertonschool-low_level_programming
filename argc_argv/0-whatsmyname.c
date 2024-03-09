2c423e343e3b7bdff7088cc2fa7c7f4ddaa3b953
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-03-09 10:54:39 +0000

    WhatsMyName??

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 
 /**
  *main - a program that print its name, followed by a new line
- *@argc: argument count 
+ *@argc: argument count
  *@argv: argument value, a strings that come after calling function
  *Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 
 	return (0);
 }

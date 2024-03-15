0e16d4075b28e8d4c7ba1aa34c892ff9aa98eab4
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-03-15 10:28:22 +0000

    Good dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - structure for a basic dog
  * @name: Dog's name
@@ -16,3 +19,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif

9dffa0ee0b9eed95315d60670d42b80fbbd31e9f
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-03-15 12:59:12 +0000

    Good dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,46 +2,52 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog structure
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- * Return: returns a pointer to newly created dog structure
+ * _strcopy - copy read only data
+ * @dst: pointer to copy char
+ * @src: read data
+ */
+void _strcopy(char *dst, char *src)
+{
+int i;
+for (i = 0; src[i]; i++)
+dst[i] = src[i];
+dst[i] = '\0';
+}
+
+/**
+ * new_dog - create new dog from the dna of the first dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
-		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	return (new_dog);
+dog_t *d;
+int a, b;
+for (a = 0; name[a]; a++)
+;
+for (b = 0; owner[b]; b++)
+;
+d = malloc(sizeof(dog_t));
+if (!d)
+return (NULL);
+d->name = malloc(a + 1);
+if (!d->name)
+{
+free(d);
+return (NULL);
+}
+d->owner = malloc(b + 1);
+if (!d->owner)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+_strcopy(d->name, name);
+_strcopy(d->owner, owner);
+d->age = age;
+return (d);
 }

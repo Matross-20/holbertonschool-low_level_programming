791c33fdc8e65bc00add3f4d14ed5746944f927b
Mthawelanga Matross <Mthawelanga Matross>
2024-03-20 13:38:29 +0000

    Numb3r print3r

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,20 +12,20 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-       va_list ap;
-       unsigned int i = 0;
-       int nums;
+va_list ap;
+unsigned int i = 0;
+int nums;
 
-       va_start(ap, n);
-       for (; i < n; i++)
-       {
-	       nums = va_arg(ap, int);
-	       printf("%d", nums);
-	       if (separator == NULL)
-	               continue;
-	       if (i < n -1)
-		       printf("%s", separator);       
-       }	       
-    printf("\n");
-    va_end(ap);
+va_start(ap, n);
+for (; i < n; i++)
+{
+nums = va_arg(ap, int);
+printf("%d", nums);
+if (separator == NULL)
+continue;
+if (i < n -1)
+printf("%s", separator);       
+}	       
+printf("\n");
+va_end(ap);
 }    

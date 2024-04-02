49c9bd8eb29423a404c0ad58036076ab881785c3
Matross-20 <matrossmthawelanga@gmail.com>
2024-04-02 14:17:10 +0000

    The number of elements

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *list_len - function returns length
+ *@h: function variable of struct/ head pointer
+ *Return: return number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num;
+
+	num = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}

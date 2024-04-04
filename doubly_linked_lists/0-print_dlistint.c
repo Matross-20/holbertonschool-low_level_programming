f882c51fa38d8c3cb2d4e51514ba8fcade701684
Matross-20 <matrossmthawelanga@gmail.com>
2024-04-04 10:34:00 +0000

    Prints all the elements of a dlistint_t list.

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 
 /**
  *print_dlistint - function prints elements in a list
- *@h: structure pointer 
+ *@h: structure pointer
  *Return: value to be returned
  */
 size_t print_dlistint(const dlistint_t *h)

8f4e691f102a55d26369434a33545a19e4b5ab57
Matross-20 <matrossmthawelanga@gmail.com>
2024-04-05 10:37:48 +0000

    Converts a binary number to an unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ *binary_to_uint -  converts a binary number to an unsigned int
+ *@b: number to convert
+ *Return: the coverted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	if (!b)
+	{
+		return (0);
+	}
+
+	while (*b != '\0')
+	{
+		i <<= 1;
+
+		if (*b == '1')
+		{
+			i |= 1;
+		}
+		else if (*b != '0')
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (i);
+}

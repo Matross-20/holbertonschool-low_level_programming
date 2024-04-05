f908f9b4ed2aa0443a18c9d6f2e7c5485f88e58f
Matross-20 <matrossmthawelanga@gmail.com>
2024-04-05 13:48:24 +0000

    Returns the number of bits you would need to flip to get from one number to another

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -6,17 +6,16 @@
  * @m: The number to set other equal
  * Return: The number of fliped bits
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flips = 0;
-	unsigned long int xor = (n ^ m);
-	unsigned long int max = 0x01;
+	unsigned long int xor_result = n ^ m;
+	unsigned int bits = 0;
 
-	while (max <= xor)
+	while (xor_result)
 	{
-		if (max & xor)
-			flips++;
-		max <<= 1;
+		xor_result &= (xor_result - 1);
+		bits++;
 	}
-	return (flips);
+	return (bits);
 }
